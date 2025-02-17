ffff35c08151a47182818ea67871a2572875db24
Rohit Kokani <148274186+Rohitkokanii@users.noreply.github.com>
2025-02-17 14:05:03 +0530

    added

--- Lab_Exercise_5_Ad_3.c
+++ Lab_Exercise_5_Ad_3.c
@@ -1,6 +1,6 @@
 //Write a program to calculate Net Salary of an employee. Accept Basic Salary (BS) from the user.
 //• HRA is 20 % of BS.
-//• DA is of BS.
+//• DA is 40% of BS.
 //• PF is 10 % of Gross Salary.
 //• Gross salary is BS+ HRA + DA.
 //Note: Net Salary = Gross Salary — PF
